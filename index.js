a105d8e9d86a31f8cc433304a6a7066967ef914b
joynalabedin05 <joynal05101993@gmail.com>
2024-03-01 21:19:01 +0600

    added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://wonderful-kheer-e59a29.netlify.app', credentials: true }));
 app.use(express.json());
 
 // MongoDB Connection URL
